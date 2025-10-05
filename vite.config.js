9977d7cd02c8f092c0f524ed1d429c4c6fd36876
Shravanimath <shravanimath@gmail.com>
2025-10-06 04:54:11 +0530

    deploy 7

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["ethers"]
+  },
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  },
   base: process.env.VITE_BASE_PATH || "/blockchain_bookmark",
 })
