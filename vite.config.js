ba85ea34564c79c0ee95b052f15d1fd9094bdbb9
Shravanimath <shravanimath@gmail.com>
2025-10-06 05:05:30 +0530

    deploy 10

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["ethers"]
-  },
+  optimizeDeps: { include: ["ethers"] },
   build: {
     rollupOptions: {
-      external: []
-    }
+      plugins: [nodePolyfills()],
+    },
   },
   base: process.env.VITE_BASE_PATH || "./",
 })
